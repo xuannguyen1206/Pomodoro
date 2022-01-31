26e52bd7253b80435509816bfc3096c4138a5ac7
xuannguyen1206 <haxuannnguyenn@gmail.com>
2022-01-31 20:10:19 +0700

    add cors

--- src/pages/api/night.ts
+++ src/pages/api/night.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 import {scrapePage} from './utils';
-
+import NextCors from 'nextjs-cors'
 type Data = {
   name: string
 }
@@ -11,9 +11,14 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
   if(color.length === 0) {
-    color = await scrapePage(`https://colorhunt.co/palettes/night`);
+    color = await scrapePage('https://colorhunt.co/palettes/night');
   }
   res.status(200).json(color as any);
 }
